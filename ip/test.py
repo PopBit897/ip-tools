7107316f80686875c823bbd591d9ba9b03c530c7
RedAnonymusITA2021 <78427215+RedAnonymusITA@users.noreply.github.com>
2021-05-31 09:44:46 +0200

    Add files via upload

---
+++ ip/test.py
@@ -0,0 +1,55 @@
+#codice tools ip red anonymus
+#tools is made italy,tools is free
+
+import socket
+import uuid
+print("***************************************************************")
+print("""
+         _              _  _ __        _              _     
+ _ _  __| | __ _       (_)| '_ \      | |_  ___  ___ | | ___
+| '_|/ _` |/ _` |      | || .__/      |  _|/ _ \/ _ \| |(_-/
+|_|  \__/_|\__/_|      |_||_|          \__|\___/\___/|_|/__/
+
+
+             """)
+print("****************************************************************\n")
+print("""my ip=1
+my hostname=2
+my  ip and hostname=3
+scanerip=4
+info=5
+web ip=6
+mac addres=7
+github link=8\n""")
+Search=input("Search ")
+link="https://github.com/RedAnonymusITA/tools-ip"
+hostname=socket.gethostname()
+ip_addres= socket.gethostbyname(hostname)
+
+if Search=="2":
+    print("you host name pc is: "+hostname)
+    cerca=input("Search ")
+    
+if Search=="1":
+    print("you ip pc: "+ip_addres)
+    cerca=input("Search ")
+    
+elif Search=="3":
+    print(hostname+" = "+ip_addres)
+    Search=input("Search ")
+if Search=="4":
+    print(scaner)
+if Search=="7":
+     print(hex(uuid.getnode()))
+if Search=="5":
+     print(""" 
+▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█░▄▄▀█░██░███░▄▄▀█░▄▀█░▄▄▀
+█░▄▄▀█░▀▀░███░▀▀▄█░█░█░▀▀░
+█▄▄▄▄█▀▀▀▄███▄█▄▄█▄▄██▄██▄
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+""")
+
+if Search =="8":
+    print(link)
+
